531dd410c8c0c894851b35eb8d1e94dd33436d8d
Gajendar <gyrao72@gmail.com>
2021-03-25 23:17:37 +0530

    router

--- basic/src/components/MainComponent.js
+++ basic/src/components/MainComponent.js
@@ -1,8 +1,12 @@
 import React,{ Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponenet';
+import Home from './HomeComponent';
+import { Switch,Route,Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -11,26 +15,37 @@ class Main extends Component {
 
         this.state={
             dishes:DISHES,
-            selectedDish:null
+            // selectedDish:null
         };
     }
     
-    onDishSelect(dishId){
-        this.setState({selectedDish:dishId});
-    }
+    // onDishSelect(dishId){
+    //     this.setState({selectedDish:dishId});
+    // }
 
     render(){
+
+        const HomePage=()=>{
+            return(
+                <Home />
+            )
+        }
+
         return (
             <div >
-                <Navbar dark color="dark" >
-                    <div className="container">
-                    <NavbarBrand href="/">My Brand</NavbarBrand>
-                    </div>
-                </Navbar>
-                
-                <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)} />
+                <Header />
 
-                <DishDetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+                {/* <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)} /> */}
+
+                {/* <DishDetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} /> */}
+
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
+
+                <Footer />
             </div>
         );
     }
